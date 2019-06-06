ee48bf2e663b8abfd55445edb3e5fb74906c537b
Abdul <awsayed@alaska.edu>
2019-06-06 03:25:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+ 
+class Dog
+
+  # setter/writer method
+  def name=(name)
+    @name = name
+  end
+
+  #getter/reader method
+  def name
+    @name
+  end
+
+  # setter/writer method
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #getter/reader method
+  def breed
+    @breed
+  end
+end
